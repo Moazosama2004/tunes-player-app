82774584f6339324fc65d2fcc62b294ad9719d0e
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2025-09-10 22:06:24 +0300

    display tune items

--- lib/views/tune_view.dart
+++ lib/views/tune_view.dart
@@ -4,6 +4,16 @@ import 'package:tunes_player_app/widgets/tune_item.dart';
 class TuneView extends StatelessWidget {
   const TuneView({super.key});
 
+  final List<Color> colors = const [
+    Color(0xffed2c3c),
+    Color(0xfff39431),
+    Color(0xfffaf35e),
+    Color(0xff3dc25a),
+    Color(0xff0da686),
+    Color(0xff0da1e7),
+    Color(0xff9911a6),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,14 +26,11 @@ class TuneView extends StatelessWidget {
         elevation: 0.0,
         centerTitle: true,
       ),
-      body: const Column(
-        children: [
-          TuneItem(color: Colors.red),
-          TuneItem(color: Colors.green),
-          TuneItem(color: Colors.redAccent),
-          TuneItem(color: Colors.amber),
-          TuneItem(color: Colors.red),
-        ],
+      body: Column(
+        children: List.generate(
+          colors.length,
+          (index) => TuneItem(color: colors[index]),
+        ),
       ),
     );
   }
